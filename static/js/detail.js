e87361b5a33de745ff907258d8383cf8153df63a
jihyun Yun <jihyun9842@gmail.com>
2023-02-12 01:16:12 +0900

    삭제완-

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,14 +1,1 @@
-let articles_pk = document.querySelector('#articles_pk').textContent
-function deletepage(articles_pk) {
-  $.ajax({
-    type: "POST",
-    url: "/detail/delete",
-    data: {
-      deletepk_give: articles_pk,
-    },
-    success: function (response) {
-      alert(response["msg"]);
-      window.location.href = '/';
-    },
-  });
-}
+
